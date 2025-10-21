f8937c9ff3f9bc765a157726ea99e09e19eee762
Moh. Su'aidi <moheedsuaidi@gmail.com>
2025-10-21 21:34:07 +0700

    chore: initial commit

---
+++ src/middlewares/upload.js
@@ -0,0 +1,21 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const filename = `product-${Date.now()}${ext}`;
+    cb(null, filename);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png"];
+  if (allowedTypes.includes(file.mimetype)) cb(null, true);
+  else cb(new Error("Invalid file type. Only JPEG, JPG, and PNG are allowed."));
+};
+
+const upload = multer({ storage: storage, fileFilter });
---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+
+// load .env
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// routes
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
